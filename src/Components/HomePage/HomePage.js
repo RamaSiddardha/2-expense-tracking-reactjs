3c5c22d26ccd5038b0fe061e3a2af1a9cf9e5957
RmaSiddardha <ramasiddardha@gmail.com>
2023-07-17 12:42:10 +0530

    Verify User Email

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,9 +1,44 @@
+import { useContext } from "react";
+import { Button } from "react-bootstrap";
+import LoginContext from "../../Context/LoginContext";
+import { json } from "react-router-dom";
 
+const HomePage = (props) => {
+  const loginCtx = useContext(LoginContext);
 
-const HomePage = props =>{
-return <>
-<h1>Welcome To Expense Tracker</h1>
-</>
-}
+  const verifyMailHandeler = async () => {
+    const result = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyAw8FdFxlRKxLBZz6ifYCdKP9Jqj8Vmslk",
+      {
+        method: "post",
+        body: JSON.stringify({
+          requestType: "VERIFY_EMAIL",
+          idToken: loginCtx.token,
+        }),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
 
-export default HomePage
+    if (result.ok) {
+      console.log("Verification Mail Sent to Your Mail I'd");
+    }
+
+    const data = await result.json();
+
+    // console.log(result)
+    // console.log(data )
+  };
+
+  return (
+    <>
+      <Button
+        onClick={verifyMailHandeler}
+        style={{ position: "absolute", top: "20%", left: "45%" }}
+      >
+        Verify Mail I'd
+      </Button>
+    </>
+  );
+};
+
+export default HomePage;
